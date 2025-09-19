65bf0f5c928474bf75efd2da56a827b211ee7c18
Helodity <228972983+Helodity-M@users.noreply.github.com>
2025-09-19 02:12:32 -0400

    Video 05: Bool

--- Assets/Scripts/Player/Delivery.cs
+++ Assets/Scripts/Player/Delivery.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    bool hasPackage = false;
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Pickup"))
         {
             Debug.Log("Grabbed Package");
+            hasPackage = true;
         }
-        if (collision.CompareTag("Customer"))
+        if (hasPackage && collision.CompareTag("Customer"))
         {
             Debug.Log("Touched Customer");
+            hasPackage = false;
         }
     }
 }
