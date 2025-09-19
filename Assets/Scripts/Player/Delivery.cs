69bae1f695c23cbd6bd928dfe8bdbc43a02e8791
Helodity <228972983+Helodity-M@users.noreply.github.com>
2025-09-19 02:26:48 -0400

    Video08: Get Component

--- Assets/Scripts/Player/Delivery.cs
+++ Assets/Scripts/Player/Delivery.cs
@@ -2,18 +2,28 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] ParticleSystem PackageParticles;
+
     bool hasPackage = false;
+
+    private void Awake()
+    {
+        PackageParticles.Stop();
+    }
+
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (!hasPackage && collision.CompareTag("Pickup"))
         {
             Debug.Log("Grabbed Package");
             hasPackage = true;
+            PackageParticles.Play();
             Destroy(collision.gameObject, 0.2f);
         }
         if (hasPackage && collision.CompareTag("Customer"))
         {
             Debug.Log("Touched Customer");
+            PackageParticles.Stop();
             hasPackage = false;
         }
     }
