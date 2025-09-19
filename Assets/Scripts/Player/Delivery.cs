7c8eec6ff2b9184089ac3081850f4cf3bdd765c2
Helodity <228972983+Helodity-M@users.noreply.github.com>
2025-09-19 02:20:05 -0400

    Video 07: Destroy Object

--- Assets/Scripts/Player/Delivery.cs
+++ Assets/Scripts/Player/Delivery.cs
@@ -5,10 +5,11 @@ public class Delivery : MonoBehaviour
     bool hasPackage = false;
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Pickup"))
+        if (!hasPackage && collision.CompareTag("Pickup"))
         {
             Debug.Log("Grabbed Package");
             hasPackage = true;
+            Destroy(collision.gameObject, 0.2f);
         }
         if (hasPackage && collision.CompareTag("Customer"))
         {
